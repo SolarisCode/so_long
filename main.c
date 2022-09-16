4756c12c634efdc0a5c50425d69d44bcfc202ad1
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2022-09-17 00:35:13 +0200

    understand mlx

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 21:27:02 by melkholy          #+#    #+#             */
-/*   Updated: 2022/09/14 23:31:34 by melkholy         ###   ########.fr       */
+/*   Updated: 2022/09/17 00:29:46 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,24 +35,47 @@ int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
+	// t_data	im g;
 	int		count;
+	int		height;
+	int		width;
+	char	*skull;
+	void	*im_sk;
+	char	**str;
 
+
+	str = (char **)malloc(11 * sizeof(char *));
+	str[0] = "11111111111111111111";
+	str[1] = "10000000000000000001";
+	str[2] = "10000000000000000001";
+	str[3] = "10000000000000000001";
+	str[4] = "10000000000000000001";
+	str[5] = "10000000000000000001";
+	str[6] = "10000000000000000001";
+	str[7] = "10000000000000000001";
+	str[8] = "10000000000000000001";
+	str[9] = "11111111111111111111";
+	str[10] = NULL;
 	count = 0;
+	int i = 0;
+	skull = "./wall.xpm";
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hey it's me");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bites_per_pixel, \
-									&img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	// while (count < 100)
-	// {
-	mlx_put_image_to_window(mlx, mlx_win, img.img, count + 1, count + 1);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, count + 2, count + 2);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, count + 3, count + 3);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, count + 4, count + 4);
-		// count ++;
-	// }
+	mlx_win = mlx_new_window(mlx, 20 * 64, 10 * 64, "Hey it's me");
+	while (i < 10 * 64)
+	{
+		count = 0;
+		while (count < 20 * 64)
+		{
+			if (str[i / 64][count / 64] == '1')
+				skull = "./wall.xpm";
+			else if (str[i / 64][count / 64] == '0')
+				skull = "./space.xpm";
+			im_sk = mlx_xpm_file_to_image(mlx, skull, &width, &height);
+			mlx_put_image_to_window(mlx, mlx_win, im_sk, count, i);
+			count += 64;
+		}
+		i += 64;
+	}
 	mlx_loop(mlx);
 	return (0);
 }
