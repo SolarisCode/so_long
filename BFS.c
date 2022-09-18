c415f3b282557e93dffbab6b99973acad9544fc4
melkholy <28461793+SolarisCode@users.noreply.github.com>
2022-09-18 18:54:57 +0200

    BFS bug fixed

--- BFS.c
+++ BFS.c
@@ -6,7 +6,7 @@
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 00:06:21 by melkholy          #+#    #+#             */
-/*   Updated: 2022/09/18 01:32:47 by melkholy         ###   ########.fr       */
+/*   Updated: 2022/09/18 18:54:06 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,29 @@ bool	ft_issafe(int r, int c)
 	return (false);
 }
 
-bool	ft_find_path(char **map, int row, int column, int **visited)
+bool	ft_find_path(char **map, int row, int col, int **visited)
 {
 	bool	up;
 	bool	down;
 	bool	right;
 	bool	left;
 
-	if (ft_issafe(row, column) && map[row][column] != 1 \
-			&& !visited[row][column])
+	if (ft_issafe(row, col) && map[row][col] != '1' \
+			&& !visited[row][col])
 	{
-		visited[row][column] = 1;
-		if (map[row][column] == 'E')
+		visited[row][col] = 1;
+		if (map[row][col] == 'E')
 			return (true);
-		right = ft_find_path(map, row, column + 1, visited);
+		right = ft_find_path(map, row, col + 1, visited);
 		if (right)
 			return (true);
-		left = ft_find_path(map, row, column - 1, visited);
+		left = ft_find_path(map, row, col - 1, visited);
 		if (left)
 			return (true);
-		up = ft_find_path(map, row - 1, column, visited);
+		up = ft_find_path(map, row - 1, col, visited);
 		if (up)
 			return (true);
-		down = ft_find_path(map, row + 1, column, visited);
+		down = ft_find_path(map, row + 1, col, visited);
 		if(down)
 			return (true);
 	}
@@ -55,12 +55,12 @@ bool	ft_ispath(char **map)
 	int		**visited;
 	int		count;
 	bool	found;
-	int		column;
+	int		col;
 	int		row;
 
 	count = 0;
 	row = 0;
-	column = 0;
+	col = 0;
 	found = false;
 	visited = (int **)calloc(10, sizeof(int *));
 	while (count < 10)
@@ -70,18 +70,18 @@ bool	ft_ispath(char **map)
 	}
 	while (row < 10)
 	{
-		column = 0;
-		while (column < 20)
+		col = 0;
+		while (col < 20)
 		{
-			if (map[row][column] == 'P' && !visited[row][column])
+			if (map[row][col] == 'P' && !visited[row][col])
 			{
-				if (ft_find_path(map, row, column, visited))
+				if (ft_find_path(map, row, col, visited))
 				{
 					found = true;
 					break ;
 				}
 			}
-			column ++;
+			col ++;
 		}
 		row ++;
 	}
@@ -102,9 +102,9 @@ int	main(void)
 	str[3] = "10000000000100000001";
 	str[4] = "1000000P000100000001";
 	str[5] = "10000000000111000001";
-	str[6] = "10000000001111000001";
-	str[7] = "10000111111000000001";
-	str[8] = "1E000000001110000001";
+	str[6] = "10000000000100000001";
+	str[7] = "10000111101E10000001";
+	str[8] = "10000000000100000001";
 	str[9] = "11111111111111111111";
 	str[10] = NULL;
 	found = ft_ispath(str);
